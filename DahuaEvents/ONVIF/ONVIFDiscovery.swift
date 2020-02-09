1c96e315296aacf9b853186ae87858574d3b5ff1
ms2138 <m38371@gmail.com>
2020-02-09 14:58:22 -0800

    Add ONVIF group.  Add ONVIFDiscovery, ONVIFQueryService, and ONVIFRequest files to project

---
+++ DahuaEvents/ONVIF/ONVIFDiscovery.swift
@@ -0,0 +1,21 @@
+//
+//  ONVIFDiscovery.swift
+//  ONVIFDiscovery
+//
+//  Created by mani on 2019-12-02.
+//  Copyright © 2019 mani. All rights reserved.
+//
+
+import Foundation
+
+struct ONVIFDiscovery {
+    let model: String
+    let ipAddress: String
+    let deviceService: URL
+}
+
+extension ONVIFDiscovery: Equatable {
+    static func == (lhs: ONVIFDiscovery, rhs: ONVIFDiscovery) -> Bool {
+        return lhs.ipAddress == rhs.ipAddress
+    }
+}
---
+++ DahuaEvents/ONVIF/ONVIFRequest.swift
@@ -0,0 +1,14 @@
+//
+//  ONVIFRequest.swift
+//  ONVIFDiscovery
+//
+//  Created by mani on 2019-12-02.
+//  Copyright © 2019 mani. All rights reserved.
+//
+
+import Foundation
+
+struct ONVIFRequest {
+    let url: URL
+    let body: Data
+}
