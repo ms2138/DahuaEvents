418caf6047080029d65eccf8bef93b631d763ba0
ms2138 <m38371@gmail.com>
2020-02-09 20:06:18 -0800

    Add EventsViewController to project

---
+++ DahuaEvents/Controllers/EventsViewController.swift
@@ -0,0 +1,28 @@
+//
+//  EventsViewController.swift
+//  DahuaEvents
+//
+//  Created by mani on 2020-02-09.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import UIKit
+
+class EventsViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+}
