d406c9181c445ea4d1f5ff2c54411db4f55c262b
ms2138 <m38371@gmail.com>
2020-02-11 20:11:30 -0800

    Add Channel struct.  Add name and number properties to Channel struct

---
+++ DahuaEvents/Models/Channel.swift
@@ -0,0 +1,14 @@
+//
+//  Channel.swift
+//  DahuaEvents
+//
+//  Created by mani on 2020-02-11.
+//  Copyright © 2020 mani. All rights reserved.
+//
+
+import Foundation
+
+struct Channel: Codable {
+    var name: String
+    var number: String
+}
--- DahuaEvents/Models/Event.swift
+++ DahuaEvents/Models/Event.swift
@@ -3,7 +3,7 @@
 //  DahuaEvents
 //
 //  Created by mani on 2020-02-09.
-//  Copyright © 2020 home. All rights reserved.
+//  Copyright © 2020 mani. All rights reserved.
 //
 
 import Foundation
