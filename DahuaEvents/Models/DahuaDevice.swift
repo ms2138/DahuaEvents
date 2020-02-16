b94a4a489fb265504beb237ccc79319b5c61b3e7
ms2138 <m38371@gmail.com>
2020-02-16 15:38:17 -0800

    Add DahuaDevice struct to project

---
+++ DahuaEvents/Models/DahuaDevice.swift
@@ -0,0 +1,13 @@
+//
+//  DahuaDevice.swift
+//  DahuaEvents
+//
+//  Created by mani on 2020-02-16.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import Foundation
+
+struct DahuaDevice {
+    
+}
