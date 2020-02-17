444526b29c80d1d3eb8016d0eedd0901d07f0e50
ms2138 <m38371@gmail.com>
2020-02-16 20:37:12 -0800

    Add Credential struct to project

---
+++ DahuaEvents/Models/Credential.swift
@@ -0,0 +1,13 @@
+//
+//  Credential.swift
+//  DahuaEvents
+//
+//  Created by mani on 2020-02-16.
+//  Copyright © 2020 home. All rights reserved.
+//
+
+import Foundation
+
+struct Credential {
+
+}
