83375061a07bea898d90be80c6ca1fd5ca3e7ff2
ms2138 <m38371@gmail.com>
2020-02-16 20:38:21 -0800

    Add username and password properties to Credential struct

--- DahuaEvents/Models/Credential.swift
+++ DahuaEvents/Models/Credential.swift
@@ -9,5 +9,11 @@
 import Foundation
 
 struct Credential {
+    var username: String
+    var password: String
 
+    init(username: String, password: String) {
+        self.username = username
+        self.password = password
+    }
 }
