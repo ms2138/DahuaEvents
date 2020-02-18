bb46804576bdb939771ec714920cadc9c1d499fe
ms2138 <m38371@gmail.com>
2020-02-17 22:31:58 -0800

    Add loadEvents method to EventsViewController

--- DahuaEvents/Controllers/EventsViewController.swift
+++ DahuaEvents/Controllers/EventsViewController.swift
@@ -28,3 +28,35 @@ extension EventsViewController {
         return events.count
     }
 }
+
+extension EventsViewController {
+    // MARK: Events
+
+    func loadEvents(for url: URL) {
+        if let urlComponents = URLComponents(url: url, resolvingAgainstBaseURL: false) {
+            if let host = urlComponents.host, let username = urlComponents.user,
+                let password = urlComponents.password {
+                if let queryItem = urlComponents.queryItems?.filter({ $0.name.contains("channel") }).first,
+                    let channel = queryItem.value {
+                    let dahuaQuery = DahuaQueryService(host: host,
+                                                       username: username,
+                                                       password: password)
+                    let startOfDay = Date().startOfDay
+                    let nextDay = startOfDay.nextDay
+
+                    dahuaQuery.getEvents(channel: channel, startTime: startOfDay.dateString(),
+                                         endTime: nextDay?.dateString() ?? startOfDay.dateString(),
+                                         directory: "/mnt/dvr/sda0", fileTypes: ["dav", "mp4"],
+                                         events: ["VideoMotion"], flags: ["Event", "Timing"],
+                                         streams: ["Main"]) { [weak self] (events, _) in
+                                            guard let weakSelf = self else { return }
+                                            if let events = events {
+                                                weakSelf.events.append(contentsOf: events)
+                                            }
+                    }
+                }
+            }
+        }
+    }
+}
+
