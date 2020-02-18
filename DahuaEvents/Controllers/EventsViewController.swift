13a92f0fc1b9b22b879ad23bc0a0300a08b7d88c
ms2138 <m38371@gmail.com>
2020-02-17 19:50:14 -0800

    Update methods numberOfSections to return 1 and numberOfRowsInSection to return the number of events.  Add videoStreamURL and events properties to EventsViewController

--- DahuaEvents/Controllers/EventsViewController.swift
+++ DahuaEvents/Controllers/EventsViewController.swift
@@ -3,26 +3,28 @@
 //  DahuaEvents
 //
 //  Created by mani on 2020-02-09.
-//  Copyright © 2020 home. All rights reserved.
+//  Copyright © 2020 mani. All rights reserved.
 //
 
 import UIKit
 
 class EventsViewController: UITableViewController {
+    var videoStreamURL: String?
+    private var events = [Event]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
+}
 
+extension EventsViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return events.count
     }
 }
